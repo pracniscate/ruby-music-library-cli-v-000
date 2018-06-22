0ff1cba5852b22bf8057f5cc78e5a99a8c054254
Lorena <lorena.giddens@outlook.com>
2018-06-22 13:16:56 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -16,5 +16,6 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    answer = gets.chomp
   end
 end
