e7dfe5f85e751379519164c8d277ae4b87f6b439
Lorena <lorena.giddens@outlook.com>
2018-06-22 13:28:34 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -35,8 +35,9 @@ class MusicLibraryController
     end
   end
 
-  def list_genres 
+  def list_genres
     Genre.all.sort_by!{ |genre| genre.name }.each_with_index do |genre, index|
       puts "#{index + 1}. #{genre.name}"
     end
+  end
 end
