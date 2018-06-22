70dd902a3b6d89672d4843288d25b8d55ebe6011
Lorena <lorena.giddens@outlook.com>
2018-06-22 13:26:33 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -30,7 +30,7 @@ class MusicLibraryController
   end
 
   def list_artists
-    Artist.all.sort_by!{ |artist| artist.name }each_with_index do |artist, index|
+    Artist.all.sort_by!{ |artist| artist.name }.each_with_index do |artist, index|
       puts "#{index + 1}. #{artist.name}"
     end
   end
