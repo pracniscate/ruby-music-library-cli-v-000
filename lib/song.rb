98ac4a639b96db52795f9a55601a586d1996daf7
Lorena <lorena.giddens@outlook.com>
2018-06-22 13:00:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,8 +49,7 @@ class Song
   end
 
   def self.create_from_filename
-    # new_from_filename(filename).tap{ |s| s.save }
-    self.new_from_filename(filename).save
+    new_from_filename(filename).tap{ |s| s.save }
   end
 
 end
