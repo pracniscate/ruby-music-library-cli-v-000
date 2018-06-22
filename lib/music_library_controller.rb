27bc2d2bb31d5689fbf3f7fa376be93307dfd6f6
Lorena <lorena.giddens@outlook.com>
2018-06-22 13:19:52 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,7 +9,7 @@ class MusicLibraryController
   def call
     puts "Welcome to your music library!"
 
-    loop do 
+    loop do
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
       puts "To list all of the genres in your library, enter 'list genres'."
