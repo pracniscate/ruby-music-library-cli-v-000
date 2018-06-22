48c0785ca63ffeec7a536b671b4fca4fe33fb1e8
Lorena <lorena.giddens@outlook.com>
2018-06-22 13:22:28 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -23,7 +23,7 @@ class MusicLibraryController
     end
   end
 
-  def list_songs 
+  def list_songs
     Song.all.each_with_index do |song, index|
       puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
